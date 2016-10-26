975dbcc5fc9e48eb0872b36bee5f361af4020c30
maolin <1120995684@qq.com>
2016-10-26 10:42:10 +0800

    更改ScrollingActivity1

--- app/src/main/java/com/raisound/rxjava/ScrollingActivity.java
+++ app/src/main/java/com/raisound/rxjava/ScrollingActivity.java
@@ -42,7 +42,6 @@ public class ScrollingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         testTv = (TextView) findViewById(R.id.test);
         setSupportActionBar(toolbar);
-        String hahah;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
